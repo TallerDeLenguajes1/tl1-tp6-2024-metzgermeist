9ec7b30e6e3bb3bd5f9cdadaaba4e4289775bae4
metzgermeist <javiermas.unt@gmail.com>
2024-05-26 16:43:13 -0300

    kj

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
 Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=5;
+Console.WriteLine("el valor de a es: "+a);
+Console.WriteLine("el valor de b es: "+b);
